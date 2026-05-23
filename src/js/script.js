10e6ff0e2a1ad126f2cec73f1681e53fa03d439e
Matheus Cunha <cunha.matheus2008@gmail.com>
2026-05-22 22:32:28 -0300

    feat: paginas home sobre e contato

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,20 @@ const themeToogle = document.getElementById("theme");
     themeToogle.addEventListener('click', ()=>{
         document.body.classList.toggle("light-mode")
     })
+
+    window.onload = function(){
+    const intro = document.getElementById("inicio");
+
+    setTimeout(() => {
+        intro.classList.add("efeito-out");
+        setTimeout(() => {
+            intro.style.display = "none";
+        }, 1000);
+    }, 3000);
+}
+
+const botaoportifolio = document.getElementById("btn-main");
+
+botaoportifolio.addEventListener("click", function() {
+    window.location.href = "./pages/portifolio.html";
+});
--- src/js/sobre.js
+++
@@ -1,4 +0,0 @@
-const themeToogle = document.getElementById("theme");
-    themeToogle.addEventListener('click', ()=>{
-        document.body.classList.toggle("light-mode")
-    })
